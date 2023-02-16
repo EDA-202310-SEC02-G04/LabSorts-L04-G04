d7d8e068bdddc34abf5c15a0f157c41e70ddd5fe
PabloCastrillon <p.castrillon@uniandes.edu.co>
2023-02-16 09:20:50 -0500

    comm

--- App/model.py
+++ App/model.py
@@ -231,10 +231,11 @@ def compareratings(book1, book2):
     # TODO modificar operador de comparacion lab 4
     return (float(book1["average_rating"]) > float(book2["average_rating"]))
 
-
+            
 # Funciones de ordenamiento
 
 def sortBooks(catalog, size):
     # TODO completar los cambios del return en el sort para el lab 4 (Parte 1).
     sub_list = lt.subList(catalog["books"], 1, size)
-    sa.sort(sub_list, compareratings)
+    sorted_list=sa.sort(sub_list, compareratings)
+    return sorted_list
